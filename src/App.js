717bfcb9292b9f2c8470fbb3bb56b97e72528550
Olayinka Fagbenro <olayinkafagbenro@gmail.com>
2023-05-30 15:12:11 +0100

    Board Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 // import logo from "./logo.svg";
 // import "./App.css";
-// import BG from "./Utils/BG.jpg";
+import BG from "./Utils/BG.jpeg";
+import Tile from "./Utils/Tile";
 
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="bg-red-400 h-screen w-full bg-cover bg-center relative grid place-items-center">
+      <img
+        src={BG}
+        className="h-screen w-full absolute object-cover"
+        alt="Background"
+      />
+      <div className="bg-transparent/50 rounded-lg relative w-96 h-96 grid grid-cols-3">
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Utils/Tile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tile = (props) => {
+  return (
+    <div className="grid place-items-center border border-slate-300 rounded-lg">
+      <p className="text-6xl font-medium text-blue-700">{props.value}</p>
+    </div>
+  );
+};
+
+export default Tile;
